c94d9e3d80097c92de8efc4b009cac76abbf8289
Liam Got Pop <gotpop@users.noreply.github.com>
2019-10-09 17:02:16 +0100

    Develop (#12)
    * Added git keep files
    
    * Adding readme
    
    * Updated readme
    
    * Added partials
    
    * Edited single pages
    
    * Adding assets
    
    * Adding ignore and package json
    
    * Adding static assets
    
    * Added colour vars
    
    * Adding babel
    
    * Ignore
    
    * Draft flags
    
    * Added package
    
    * Added basic header menu
    
    * Updated list
    
    * Ad block tag
    
    * Updated content
    
    * Updated content
    
    * All pages in header nav
    
    * About page section template
    
    * Upodating nav
    
    * Updated partials
    
    * Menu and icons
    
    * Added netlify cms
    
    * Added netlify cms
    
    * Readme
    
    * Moved admin
    
    * Added collections in settings
    
    * Added default js
    
    * Added collections
    
    * Adding work collection
    
    * Intersect
    
    * Remove workflow

--- themes/magnatone/assets/js/components/intersect.js
+++ themes/magnatone/assets/js/components/intersect.js
@@ -1,28 +1,25 @@
-let options = {
-    root: document.querySelector('#scrollArea'),
-    rootMargin: '0px',
-    threshold: 1.0
-}
+// let options = {
+//     root: document.querySelector('#scrollArea'),
+//     rootMargin: '0px',
+//     threshold: 1.0
+// }
 
-let observer = new IntersectionObserver(callback, options);
-<<<<<<< HEAD
+// let observer = new IntersectionObserver(callback, options);
 
-document.addEventListener("DOMContentLoaded", function () {
-    const imageObserver = new IntersectionObserver((entries, imgObserver) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                const lazyImage = entry.target
-                console.log("lazy loading ", lazyImage)
-                lazyImage.src = lazyImage.dataset.src
-                lazyImage.classList.remove("lzy_img");
-                imgObserver.unobserve(lazyImage);
-            }
-        })
-    });
-    const arr = document.querySelectorAll('img.lzy_img')
-    arr.forEach((v) => {
-        imageObserver.observe(v);
-    })
-});
-=======
->>>>>>> 4f3ab1efc45c98a463f508e34d499bdda1ecdf29
+// document.addEventListener("DOMContentLoaded", function () {
+//     const imageObserver = new IntersectionObserver((entries, imgObserver) => {
+//         entries.forEach((entry) => {
+//             if (entry.isIntersecting) {
+//                 const lazyImage = entry.target
+//                 console.log("lazy loading ", lazyImage)
+//                 lazyImage.src = lazyImage.dataset.src
+//                 lazyImage.classList.remove("lzy_img");
+//                 imgObserver.unobserve(lazyImage);
+//             }
+//         })
+//     });
+//     const arr = document.querySelectorAll('img.lzy_img')
+//     arr.forEach((v) => {
+//         imageObserver.observe(v);
+//     })
+// });
